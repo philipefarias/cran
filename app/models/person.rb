38f2fe0d4bcb9b2743b098cc42bad899076a62d2
Philipe Farias <philipefarias@gmail.com>
2015-07-04 16:30:51 -0300

    Adding uniqueness validation to Person name
    I know, I know...
    It isn't right but there's no other info that can be used for that,
    because not everyone has an email.

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,5 @@ class Person < ActiveRecord::Base
   has_and_belongs_to_many :maintained_packages, class_name: "Package", join_table: "packages_maintainers"
 
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
