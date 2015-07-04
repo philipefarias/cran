607d53e726120d3b0a7aa6c7cab20bedf2eba4b5
Philipe Farias <philipefarias@gmail.com>
2015-07-04 15:42:36 -0300

    Adding lib to Rails autoload paths

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module Cran
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths += Dir["#{config.root}/app/services", "#{config.root}/lib"]
   end
 end
